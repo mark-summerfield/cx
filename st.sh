ec26f6f7530e8013144d3e7e67f911ce04643e45
mark-summerfield <mark@qtrac.eu>
2024-09-12 18:27:31 +0100

    m

--- st.sh
+++ st.sh
@@ -1,23 +1,7 @@
 rm -f core
 clc -sL rb
-FILES=( 
-    set_str.c
-    set_str_test.c
-    set_int.c
-    set_int_test.c
-    cx_test.c
-    cx_util_test.c
-    sx.c
-    sx_test.c
-    va_test.c
-    vec.c
-    vec_common_test.c
-    vec_int.c
-    vec_int_test.c
-    vec_str.c
-    vec_str_test.c
-    vec_test.c
-)
+FILES=(*.c)
 cppcheck -q --enable=warning --enable=style --enable=performance \
     --enable=portability --enable=unusedFunction "${FILES[@]}"
+malloc_check.py "${FILES[@]}"
 git st
